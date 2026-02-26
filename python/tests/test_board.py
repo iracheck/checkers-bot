7cfe737624e348c3f0fa3a87975a2ad4fb78fcda
iracheck <irarcheck@gmail.com>
2026-02-26 16:13:11 -0500

    Implement new move() unit test

--- python/tests/test_board.py
+++ python/tests/test_board.py
@@ -1,6 +1,15 @@
 from game.board import Board
 from game.piece import Piece
 
+def test_move():
+    board = Board()
+    board.clear()
+
+    # manually set up a piece at the edge of the board
+    board.set(0, 1, Piece(Piece.WHITE))
+    board.move(0, 1, 1, 0)
+
+    assert board.get(1, 0) is not None
 
 def test_promotion():
     board = Board()
